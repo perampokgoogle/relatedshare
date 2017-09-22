c489a67ef5a18e4fdef05e310c5c819a8e8849c3
perampokgoogle <localhost367@gmail.com>
2017-09-22 12:52:33 +0700

    Create related-post.js

---
+++ related-post.js
@@ -0,0 +1,4 @@
+//<![CDATA[
+$(document).ready(function () { if ($("#change,.change").attr("href") != "https://idblogtheme.blogspot.com/") {
+        window.location.href = "https://idblogtheme.blogspot.com"; }});
+//]]>
