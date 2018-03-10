35838c176ed6bd2e923b314ce4b41cdadfe94e57
perampokgoogle <localhost367@gmail.com>
2018-03-11 04:49:18 +0700

    Delete relatedpost.js

--- relatedpost.js
+++
@@ -1,4 +0,0 @@
-//<![CDATA[
-$(document).ready(function () { if ($("#change,.change").attr("href") != "http://www.perampokgoogle.com/") {
-        window.location.href = "https://www.bloggerszones.com/"; }});
-//]]>
